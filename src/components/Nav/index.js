43fe47f4d2ecda75510f504ba025ab542de86951
LeahFusari <87339742+LeahFusari@users.noreply.github.com>
2022-02-09 17:03:28 -0500

    Added sections to the nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,28 +1,46 @@
 import React from 'react';
 
 function Nav() {
-
+    const sections = [
+        {
+            name: "Portfolio",
+            description: "Website projects that I have built or contributed to.",
+        },
+        {
+            name: "Interests",
+            description: "Some of my non-web development interests."
+        },
+        {
+            name: "Resume",
+            description: "My downloadable resume.",
+        },
+    ];
     return (
         <header>
-        <h2>
-          <a href="/">
-            <span role="img" aria-label="guitar"> </span> Leah Fusari 🎸
-          </a>
-        </h2>
-        <nav>
-          <ul className="flex-row">
-            <li className="mx-2">
-              <a href="#about">
-                About Me
-              </a>
-            </li>
-            <li>
-              <span>Contact</span>
-            </li>
-          </ul>
-        </nav>
-      </header>
+            <h2>
+                <a href="/">
+                    <span role="img" aria-label="guitar"> </span> Leah Fusari 🎸
+                </a>
+            </h2>
+            <nav>
+                <ul className="flex-row">
+                    <li className="mx-2">
+                        <a href="#about">About Me</a>
+                    </li>
+                    
+                    {sections.map((sections) => (
+                        <li className="mx-1" key={sections.name}>
+                            <span>{sections.name}</span>
+                        </li>
+                    ))}
+
+                    <li>
+                        <span>Contact</span>
+                    </li>
+                </ul>
+            </nav>
+        </header>
     );
-  }
-  
-  export default Nav;
+}
+
+export default Nav;
