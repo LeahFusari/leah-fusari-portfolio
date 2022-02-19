84fb7812845f3d92a4da2f605b0a9e4e66aa1b9a
LeahFusari <87339742+LeahFusari@users.noreply.github.com>
2022-02-19 13:09:10 -0500

    more styling with footer

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,18 +8,12 @@ function About() {
         <h1 className="view-name">About Me</h1>
       <section className="my-5">
         
-        <img src={profileImage} className="my-2" alt="cover" />
+        <img src={profileImage} className=" p-img my-2" alt="cover" />
         <div id= "about-text" className="container-md">
-          <p>I am an experienced professional with proficiency in coordinating a variety of projects, managing training operations, and working with a wide customer base.
-            <br></br> <br></br> 
-            My knowledge includes providing technical training segments to new and existing employees, handling all aspects of operations site management, and serving as chief communication liaison.
-            <br></br> <br></br> 
-            I serve as a front line for clients, consumers, retailers and manufacturers with in-depth knowledge of various program components. I have performed multiple human resources and accounting functions and my ability to multi-task in a deadline-driven environment allows me to attain and exceed objectives.
-            <br></br> <br></br> 
-            I will be attending coding school in Sept 2021 with the goal of attaining a certificate in Full Stack coding and a career change to the Tech Industry.
-            <br></br> <br></br> 
-            My personal interests include but are not limited to:
-            Music, rockhounding, camping, fishing, martial arts and visiting the mountains whenever possible.
+          <p>Hello!  My name is Leah Fusari and I am currently enrolled in the University of Connecticut School of Engineering Coding Bootcamp to become a Full Stack Web Developer.  Please follow the link above to see my full resume and web proficiencies.
+            <br></br> <br></br>
+            In addition to my web development skills, I have served as a front line for clients, consumers, retailers and manufacturers with in-depth knowledge of various program components. I have performed multiple human resources and accounting functions and my ability to multi-task in a deadline-driven environment allows me to attain and exceed objectives.
+            
           </p>
         </div>
       </section>
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,10 +3,14 @@ import React from 'react';
 function Footer() {
     return (
       <footer>
-          <h3>Thank you for visiting! Please check out my links!</h3>
+      <div>
+          <h4>Thank you for visiting! Please check out my links!</h4>
+      </div>
+      <div className='foot-anchor-container'>
         <a href= "https://github.com/LeahFusari" target="_blank" rel="noreferrer">Github</a>
         <a href= "https://www.linkedin.com/in/leah-fusari-79b35821b/" target="_blank" rel="noreferrer">LinkedIn</a>
         <a href= "https://stackoverflow.com/users/18217848/leah-fusari" target="_blank" rel="noreferrer">StackOverflow</a>
+      </div>
       </footer>
     );
   }
